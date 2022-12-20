1f75005c47868db2666f094f97623e91b0b2f93c
Hồng Phong Phạm <87419984+phongp3j@users.noreply.github.com>
2022-12-20 11:46:34 +0700

    Update thuchanh1.py

--- thuchanh1.py
+++ thuchanh1.py
@@ -1,11 +1,1 @@
-m , n ,p = map(int,input().split())
-a = []
-for i in range(m):
-    b = list(map(int,input().split()))
-    a.append(b)
-dem = 0
-for i in range(m*p):
-    for j in range(n*p):
-        if i < dem*p + m:
-            if j < dem*p+n:
-                print
+
